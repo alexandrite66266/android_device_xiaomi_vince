cd839d415877f8f84df42717e97a6756d9bf064a
kubersharma001 <kubersharma001@gmail.com>
2018-10-07 20:52:05 +0530

    vince: track and use DragonTC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 7.0.2
 KBUILD_COMPILER_STRING := Android clang version 7.0.2
 export KBUILD_COMPILER_STRING
-#TARGET_KERNEL_CLANG_PATH := $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/dragontc-7.0/bin
+TARGET_KERNEL_CLANG_PATH := $(PWD)/prebuilts/clang/host/$(HOST_OS)-x86/7.0-DragonTC/bin
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
